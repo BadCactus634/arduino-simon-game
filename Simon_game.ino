0e95c9a88d96cb35d6e70e39ab198c456be68082
Stefano <68558172+BadCactus634@users.noreply.github.com>
2022-10-07 10:00:10 +0200

    Update Simon_game.ino
    - Added comments (italian)

--- Simon_game.ino
+++ Simon_game.ino
@@ -1,28 +1,31 @@
-int buzzer=3;
-int pin;
+// Dichiarazioni variabili
+int buzzer=3; // Pin buzzer
+int pin; // Variabile di supporto per la generazione della sequenza random
 int livello=1;
 int estrazioni;
 int inserimento;
 int estr_old;
-int sequenza[8];
+int sequenza[8]; // L'array che terrà in memoria la sequenza generata
+
 void setup() {
-  Serial.begin(9600);
-  randomSeed(analogRead(A0));
+  Serial.begin(9600); // Inizializza la comunicazione seriale di debug
+  randomSeed(analogRead(A0)); // Inizializza il seme per la generazione della sequenza random
+  // Pin di output
   pinMode(buzzer, OUTPUT);
   pinMode(4, OUTPUT);
   pinMode(5, OUTPUT);
   pinMode(6, OUTPUT);
   pinMode(7, OUTPUT);
-  pinMode(12, INPUT);
-  pinMode(11, INPUT);
-  pinMode(10, INPUT);
+  // Pin di input (pulsanti)
   pinMode(9, INPUT);
+  pinMode(10, INPUT);
+  pinMode(11, INPUT);
+  pinMode(12, INPUT);
 }
 
 void loop() {
-
- if(livello==1 || livello==2 || livello==3){
-  inserimento=0;
+ if(livello==1 || livello==2 || livello==3){ // Esegui il gioco solo se si è in uno dei 3 livelli
+  inserimento=0; // Variabile temporanea di supporto per l'inserimento che useremo dopo
   
   // Controllo numero di estrazioni basaste sul livello
   if(livello==1)
@@ -32,12 +35,13 @@ void loop() {
   else
     estrazioni=8;
 
-  if(livello==1){
+  if(livello==1){ // Se siamo nel primo livello estraiamo 4 numeri
   for(estr_old=0;estr_old<estrazioni;estr_old++){
-    pin=random(4, 8);
+    pin=random(4, 8); // Genera un numero random compreso fra 4 e 7 (l'8 non si conta)
     Serial.println(pin);
-    sequenza[estr_old]=pin;
-    digitalWrite(pin, HIGH);
+    sequenza[estr_old]=pin; // Memorizza il numero alla posizione corrente
+    digitalWrite(pin, HIGH); // Accendi il LED corrispondente
+    // Riproduci un tono a seconda del LED estratto
     switch(pin){
       case 4:
         tone(buzzer, 1500);
@@ -57,8 +61,9 @@ void loop() {
     noTone(buzzer);
     delay(500);
   }
-  }else{ // Se il livello è diverso da 1
+  }else{ // Se il livello è diverso da 1 (2 o 3)
     Serial.println("Vecchia sequenza:");
+    // Leggi l'array con i valori memorizzati dalla precedente estrazione
     for(int i=0;i<estr_old;i++){
     Serial.println(sequenza[i]);
     digitalWrite(sequenza[i], HIGH);
@@ -81,7 +86,7 @@ void loop() {
     noTone(buzzer);
     delay(500);
     }
-    // Dopo aver stampato i vecchi valori, inventa quelli nuovi
+    // Ora estrai i due numeri nuovi e stampali
     Serial.println("Nuova estrazione:");
     for(estr_old;estr_old<estrazioni;estr_old++){
     pin=random(4, 8);
@@ -109,15 +114,12 @@ void loop() {
   }
   }
 
-//  Serial.println("------- SEQUENZA MEMORIZZATA -------");  
-//  for(int i=0;i<estrazioni;i++)
-//  Serial.println(sequenza[i]);
-
   Serial.println("------- INIZIO INSERIMENTO -------");
 
   while(inserimento<estrazioni){
     if(digitalRead(12)==HIGH && sequenza[inserimento]==6){ // Verde
       Serial.println("Pulsante verde premuto");
+      // Fai suonare il buzzer e accendere il LED corrispondente
       tone(buzzer, 1000);
       digitalWrite(6, HIGH);
       inserimento++;
@@ -152,6 +154,7 @@ void loop() {
       noTone(buzzer);
       digitalWrite(4, LOW);
     }else if(digitalRead(12)==HIGH || digitalRead(11)==HIGH || digitalRead(10)==HIGH || digitalRead(9)==HIGH){
+      // Se un tasto è premuto ma non corrisponde alla sequenza memorizzata riproduci un suono di errore
       Serial.println("PULSANTE ERRATO!");
       tone(buzzer, 300);
       delay(200);
@@ -169,6 +172,7 @@ void loop() {
   
   if(inserimento==estrazioni){
     if(livello<3){
+      // Sali di livello
       livello++;
       Serial.println("------- RAGGIUNTO NUOVO LIVELLO -------");
     }else if(livello==3){
@@ -183,10 +187,12 @@ void loop() {
           noTone(buzzer);
         }
       }
+      // Aumenta il livello oltre al 3 in modo da non rieseguire il gioco
       livello++;
     }
   }else if(inserimento==20){
     Serial.println("Ricomincia dall'inizio");
+    // Resetta il livello a 1
     livello=1;
   }
   delay(2000);
